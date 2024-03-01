c6a349da78ab6eae0be5bfa7f44bca0748afeccc
Yoshifumi Kondo <yoshifumi.kondo1993@gmail.com>
2024-03-01 18:51:53 +0900

    fix:page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout() {
   return (
     <html lang="en">
       <head>
         <title>Home</title>
       </head>
       <body>
-        {children}
         <Analytics />
         <SpeedInsights />
       </body>
