c7d76c8118b547068f7ab663b8d23ecb3174bc65
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-03 21:47:20 +0900

    Refactor OriginalText class to use a static readonly property for _TEXT_MAX_LENGTH

--- src/api/lib/domain/translator/OriginalText.ts
+++ src/api/lib/domain/translator/OriginalText.ts
@@ -1,7 +1,7 @@
 const symbol = Symbol("OriginalText");
 
 export class OriginalText {
-  private _TEXT_MAX_LENGTH = 100;
+  private static readonly _TEXT_MAX_LENGTH = 100;
   public static [symbol] = symbol;
   private _text: string;
 
@@ -10,9 +10,9 @@ export class OriginalText {
     this.validate();
   }
   private validate() {
-    if (this._text.length > this._TEXT_MAX_LENGTH) {
+    if (this._text.length > OriginalText._TEXT_MAX_LENGTH) {
       throw new Error(
-        `Text should be less than ${this._TEXT_MAX_LENGTH} characters`
+        `Text should be less than ${OriginalText._TEXT_MAX_LENGTH} characters`
       );
     }
   }
