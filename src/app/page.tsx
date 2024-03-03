415ecde2637f4267254437e8b95d21ca86847192
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-03 14:19:06 +0900

    Add analytics and speed insights components to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function RootLayout() {
   return (
     <html lang="en">
       <head>
         <title>Home</title>
       </head>
-      <body>
-        <Analytics />
-        <SpeedInsights />
-      </body>
+      <body></body>
     </html>
   );
 }
