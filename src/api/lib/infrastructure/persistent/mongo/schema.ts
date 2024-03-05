ae97da1b9043b160c191ed5befc2a79f16aadb74
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-05 21:15:11 +0900

    Add MongoDB connection in schema.ts

--- src/api/lib/infrastructure/persistent/mongo/schema.ts
+++ src/api/lib/infrastructure/persistent/mongo/schema.ts
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+const mongoDBUrl = process.env.MONGODB_URL!!;
+
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => console.log("MongoDB connection successful"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
 export const UserSchema = new mongoose.Schema({
   id: {
     type: String,
