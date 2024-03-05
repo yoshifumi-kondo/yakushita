74c28bc8feda0597bc64e2f22e064a8a5187f1c5
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-05 21:17:07 +0900

    Remove IUserRepository interface

--- src/api/lib/repository/i.user.repository.ts
+++
@@ -1,9 +0,0 @@
-import { UserAuth } from "@/api/lib/domain/user/auth/UserAuth";
-import { User } from "@/api/lib/domain/user/User";
-import { UserId } from "@/api/lib/domain/user/UserId";
-
-export abstract class IUserRepository {
-  abstract createUser(user: User): Promise<void>;
-  abstract getUser(auth: UserAuth): Promise<User>;
-  abstract deleteUser(id: UserId): Promise<void>;
-}
