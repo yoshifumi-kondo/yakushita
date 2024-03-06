3c9371b5d177bf784a387581d88293f3d64f5de6
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-06 22:25:27 +0900

    Update dependencies and add authentication components

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth";
+import { GoogleAuth } from "@/api/lib/domain/user/auth/GoogleAuth";
+import { UserAuth } from "@/api/lib/domain/user/auth/UserAuth";
+import { SignUpUsecase } from "@/api/usecase/auth/SignUpUsecase";
+import GoogleProvider from "next-auth/providers/google";
+import { SignInOrSignUpUsecase } from "@/api/usecase/auth/SingInOrSignUpUsecase";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      try {
+        if (account?.provider === "google") {
+          const createdUser = await SignInOrSignUpUsecase(
+            new UserAuth({ google: new GoogleAuth(user.id) })
+          );
+          if (createdUser) {
+            user.userId = createdUser.toJSON().id;
+            return true;
+          }
+        }
+
+        return false;
+      } catch (e) {
+        console.error(e);
+        return false;
+      }
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.userId = user.userId;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      try {
+        session.userId = token.userId as string;
+        return session;
+      } catch (e) {
+        console.error(e);
+        return session;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main></main>;
+"use client";
+import Component from "@/component/auth/SignIn";
+import { SessionProvider } from "next-auth/react";
+
+export default function PageWithSession() {
+  return (
+    <main className="w-screen h-screen flex-col flex justify-center items-center">
+      <SessionProvider session={null}>
+        <Component />
+      </SessionProvider>
+    </main>
+  );
 }
