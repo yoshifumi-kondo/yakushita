222b54e450ddfb4e81aabe470d4c06aafdc4d3fa
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-09 12:12:48 +0900

    Add loading state for session

--- src/component/auth/SignIn.tsx
+++ src/component/auth/SignIn.tsx
@@ -1,8 +1,12 @@
-"use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Component() {
   const { data: session } = useSession();
+
+  if (session === undefined) {
+    return <p>Loading...</p>;
+  }
+
   if (session) {
     return (
       <>
