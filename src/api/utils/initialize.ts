2e0d12dbbd8b22ec9d052b27a015be3e94d2a9a1
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-09 12:32:53 +0900

    Refactor initialize function to initializeServer

--- src/api/utils/initialize.ts
+++ src/api/utils/initialize.ts
@@ -1,5 +1,5 @@
 import { MongoDBConnection } from "@/api/lib/infrastructure/persistent/mongo/connection";
 
-export const initialize = async () => {
+export const initializeServer = async () => {
   await MongoDBConnection.init();
 };
