568ccd5583ac22164dede25603b87e7eab51d278
yoshifumi-kondo <yoshifumi.kondo1993@gmail.com>
2024-03-23 10:22:22 +0900

    Add new files and remove unused dependencies

--- client/src/api/service/index.ts
+++ client/src/api/service/index.ts
@@ -1,3 +1,4 @@
+import { LemmatizeService } from "@/api/lib/infrastructure/adapter/lemmatize/LemmatizeService";
 import { OpenAiService } from "@/api/lib/infrastructure/adapter/openai/OpenAiService";
 import { UserRepository } from "@/api/lib/infrastructure/persistent/mongo/UserRepository";
 import { TranslationService } from "@/api/service/translation/TranslationService";
@@ -7,4 +8,5 @@ import { ENV_KEY, getEnvValue } from "@/utils/geEnv";
 export const userService = new UserService(new UserRepository());
 export const translationService = new TranslationService(
   new OpenAiService(getEnvValue(ENV_KEY.OPENAI_API_KEY)),
+  new LemmatizeService()
 );
